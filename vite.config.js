3dc8c06b478bd5f729968ebe45e777a18afdc068
TanguyBonfitt0 <tanguy.bonfitto@supinfo.com>
2026-01-07 14:19:14 +0100

    pre-deploiemnt v3

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     // Remplace 'mon-portfolio' par le nom EXACT de ton dépôt GitHub entre les slashes
-    base: 'https://github.com/TanguyBonfitt0/portfolio-tanguy.git',
+    base: 'portfolio-tanguy',
 })
